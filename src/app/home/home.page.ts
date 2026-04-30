a7ef19d2ef8ea53e96fd85f3412f5f7e852ec0bd
manuelivanstudent <manuel.ivan1988@yahoo.com>
2026-04-30 10:13:20 +0100

    Adding favourites heart

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,34 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonList, IonItem, IonIcon } from '@ionic/angular/standalone';
+import { NgFor, NgIf } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonList, IonItem, IonIcon, IonButtons } from '@ionic/angular/standalone';
 import { ServicesData } from '../services-data';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonIcon, IonItem, IonButton, FormsModule, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, IonList, AsyncPipe],
+imports: [ IonButtons, IonIcon, IonItem, IonButton, FormsModule, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, IonList, NgFor, NgIf],
 })
 export class HomePage {
 
   movies: any[] = [];
   keyword: string = "";
+  favourites: Set<number> = new Set();
+
   constructor(private router: Router, private sd: ServicesData) {}
 
+  goToFavourites() {
+    this.router.navigate(['/favourites']);
+  }
+
   async ngOnInit() {
-    this.movies = await this.sd.getTrendingMovies();
+    const favs = await this.sd.getFavourites();
+    this.favourites = new Set(favs.map((f: any) => f.id));
+
+    const all = await this.sd.getTrendingMovies();
+    this.movies = all.slice(0, 20);
   }
 
   async openMovies() {
@@ -26,15 +36,17 @@ export class HomePage {
     this.router.navigate(['/movies']);
   }
 
-  async isFavourite(movie: any) {
-    return await this.sd.isFavourite(movie.id);
+  isFavourite(movie: any) {
+    return this.favourites.has(movie.id);
   }
 
   async toggleFavourite(movie: any) {
-    if (await this.isFavourite(movie)) {
+    if (this.isFavourite(movie)) {
       this.sd.removeFavourite(movie.id);
+      this.favourites.delete(movie.id);
     } else {
       this.sd.addFavourite(movie);
+      this.favourites.add(movie.id);
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { importProvidersFrom } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { heart, heartOutline } from 'ionicons/icons';
+
+addIcons({
+  heart,
+  heartOutline
+});
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    importProvidersFrom(IonicStorageModule.forRoot()),
-  ],
+    importProvidersFrom(IonicStorageModule.forRoot())
+  ]
 });
